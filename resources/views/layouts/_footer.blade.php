aff821867bab1048a61b4b6262fa3cded29aed99
xiaofei <zhangxiaofei1001@gmail.com>
2017-12-13 09:27:57 +0800

    header and footer

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+	<footer class="footer">
+		<small class="slogon">
+			<img class="brand-icon" src="" />
+			<a href="https://yousails.com/">
+				优帆远航 | 创造不息，交付不止
+			</a>
+		</small>
+		<nav>
+			<ul>
+				<li><a href="{{route('about')}}">关于</a></li>
+			</ul>
+		</nav>
+	</footer>
+</div>
